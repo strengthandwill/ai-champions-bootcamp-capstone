1b8b2cbaf936dd50488987dc06f2fbea2efa8b95
Poh Kah Kong <kahkong87mail@gmail.com>
2024-10-23 09:30:33 +0800

    Add Disclaimer

--- main.py
+++ main.py
@@ -12,3 +12,12 @@ st.title("🎁 Baby Benefits Calculator")
 
 st.page_link("pages/2_Baby_Bonus.py", label="Baby Bonus Calculator", icon="👶")
 st.page_link("pages/3_Preschool_Subsidies.py", label="Preschool Subsidies Calculator", icon="🏫")
+
+with st.expander("Disclaimer"):
+    st.markdown(f"""
+        **IMPORTANT NOTICE:** This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+
+        Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+
+        Always consult with qualified professionals for accurate and personalized advice.
+    """)
